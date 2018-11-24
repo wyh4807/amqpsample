8cf30bd16ee74072c249769990799536596e1532
akj.hoseo <akj.hoseo@gmail.com>
2018-11-24 15:39:07 +0900

    sample

--- src/main/java/io/hoseo/fbsample/RabbitConfig.java
+++ src/main/java/io/hoseo/fbsample/RabbitConfig.java
@@ -1,7 +1,13 @@
 package io.hoseo.fbsample;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
